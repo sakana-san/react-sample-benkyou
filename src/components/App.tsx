5e90365a2a1b404dd3774b63d87e5a7064aab6d1
sakana-san <>
2023-05-18 13:37:46 +0900

    axiosでjsonの情報を取り入れ表示させた

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,38 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import { type } from "os";
 
+type Todos = {
+  id: number
+  content: string
+  done: boolean
+}
+
+const todoDataUrl = "http://localhost:3001/todos";
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(todoDataUrl);
+      setTodoList(response.data);
+    };
+    fetchData();
+  }, []);
   return (
-    <p>これからアプリを実装します。</p>
+    <>
+      <h1>todo進捗管理</h1>
+      <button>+ todoを追加</button>
+      <h2>todoリスト</h2>
+      <ul>
+        {
+         todoList.map((v: any) => (
+          <li key={v.id}>
+            {v.content}({v.done ? '完了' : '未完了'})
+          </li>
+         ))
+        }
+      </ul>
+    </>
   );
 }
 
