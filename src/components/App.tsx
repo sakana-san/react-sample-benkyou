d23758b74e2012cca76dd369eaf2c55c73853dc7
sakana-san <>
2023-05-18 14:13:15 +0900

    部品化した

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,14 +2,62 @@ import React, {useEffect, useState} from "react";
 import axios from "axios";
 import { type } from "os";
 
-type Todos = {
-  id: number
-  content: string
-  done: boolean
+
+type TProps = {
+  title: string
+  size: string
 }
 
-const todoDataUrl = "http://localhost:3001/todos";
+type TLProps = {
+  todoList: string[]
+}
+
+type TIProps = {
+  key: number
+  item: string
+  flag: boolean
+}
+
+
+
+const TodoTitle = (props: TProps) => {
+  const {title, size} = props
+  if (size === 'h1') return <h1>{title}</h1>
+  if (size === 'h2') return <h2>{title}</h2>
+
+  return <p>{title}</p>
+}
+
+const TodoList = (props: TLProps) => {
+  const {todoList} = props
+  return (
+    <ul>
+      {
+        todoList.map((v: any) => (
+          <TodoItem item={v.content} flag={v.done} key={v.id} />
+        ))
+      }
+    </ul>
+  )
+}
+
+const TodoItem = (props: TIProps) => {
+  const {item, flag} = props
+  return (
+    <li>
+      {item}
+      <button>{flag ? "未完了リストへ" : "完了リストへ"}</button>
+      <button>削除</button>
+    </li>
+  )
+}
+
+
 function App() {
+  // localhostはポート3000
+  // モックサーバーはポート3001
+  //　ポートを分けないと画面が真っ白になる
+  const todoDataUrl = "http://localhost:3001/todos";
   const [todoList, setTodoList] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
@@ -18,20 +66,31 @@ function App() {
     };
     fetchData();
   }, []);
+
+  // 方式 --------------------------------------------------
+  console.log('Todoリスト', todoList);  
+  const inCompList = todoList.filter((d: any) => {
+    return !d.done;
+  })
+  const compList = todoList.filter((d: any) => {
+    return d.done;
+  })
+  // --------------------------------------------------
+
+
   return (
     <>
-      <h1>todo進捗管理</h1>
+      <TodoTitle title="todo進捗管理" size="h1"/>
+      <h1></h1>
+      <textarea />
       <button>+ todoを追加</button>
-      <h2>todoリスト</h2>
-      <ul>
-        {
-         todoList.map((v: any) => (
-          <li key={v.id}>
-            {v.content}({v.done ? '完了' : '未完了'})
-          </li>
-         ))
-        }
-      </ul>
+
+      <TodoTitle title="未完成todoリスト" size="h2"/>
+      <TodoList todoList={inCompList} />
+
+
+      <TodoTitle title="完成todoリスト" size="h2"/> 
+      <TodoList todoList={compList} />
     </>
   );
 }
