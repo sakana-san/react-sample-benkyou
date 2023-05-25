0e85f6c6a7abc37f9fdb511fa5c16a538d78a5d8
sakana-san <>
2023-05-25 12:16:50 +0900

    練習ページのデザイン修正

--- src/components/Practice.tsx
+++ src/components/Practice.tsx
@@ -1,11 +1,26 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Button } from "@chakra-ui/react";
+import React from "react"
+import { Link } from "react-router-dom"
+import {  Flex, Button, Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 
 export const Practice = () => {
   return (
-    <div>
-      <Button ml="10"><Link to='/Radio'>ラジオページ</Link></Button>
-    </div>
+    <>
+    <Flex alignItems="center" justifyContent="center">
+      <Tabs variant='soft-rounded' colorScheme='green'>
+        <TabList>
+          <Tab>練習1</Tab>
+          <Tab>Tab 2</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <Button ml="10"><Link to='/Radio'>ラジオページ</Link></Button>
+          </TabPanel>
+          <TabPanel>
+            <p>two!</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </Flex>
+    </>
   )
 }
