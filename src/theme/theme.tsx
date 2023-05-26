1f30fd0cd39a402fedcdde42c20f5ce9cd439e2b
sakana-san <>
2023-05-26 17:03:04 +0900

    インデント修正

--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -2,18 +2,18 @@ import { extendTheme } from "@chakra-ui/react";
 
 
 const theme = extendTheme({
- styles: {
-  globa: {
-    body: {
-      backgroundColor: "orange.50",
-      color: "gray.80"
-    },
-    p: {
-      fontSize: {base: "md", "md": "lg"},
-      lineHeight: "tall"
+  styles: {
+    globa: {
+      body: {
+        backgroundColor: "orange.50",
+        color: "gray.80"
+      },
+      p: {
+        fontSize: {base: "md", "md": "lg"},
+        lineHeight: "tall"
+      }
     }
   }
- }
 });
 
 export default theme;
