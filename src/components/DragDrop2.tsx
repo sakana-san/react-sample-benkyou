bb878e1c778929d0699ffc3b822f252d190e9a94
sakana-san <>
2023-06-12 20:30:01 +0900

    押し込みの実装

--- src/components/DragDrop2.tsx
+++ src/components/DragDrop2.tsx
@@ -22,6 +22,9 @@ display: flex;
 .yellow {
   background-color: yellow;
 }
+.pink {
+  background-color: pink;
+}
 `;
 
 const Draggable = styled.div`
@@ -30,47 +33,95 @@ const Draggable = styled.div`
 	width: 100px;
 `;
 
-
-type classStatus = {
-  isFuyo: boolean
+type Point = {
+  x: number,
+  y: number
 }
+type MouseStatus = {
+  isDown: boolean
+  isMove: boolean
+  isUp: boolean
+}
+type DraggingEl = {
+  mouseStatus: MouseStatus
+}
+type Handler = (e: React.MouseEvent<EventTarget & HTMLElement>) => void
 
-const useDragElemetns = () => {
-  const [fuyo, setFuyo] = useState<classStatus>({
-    isFuyo: false
+const useDragElemetns = (): [DraggingEl, Handler] => {
+  const [mouseStatus, setMouseStatus] = useState<MouseStatus>({
+    isDown: false,
+    isMove: false,
+    isUp: false
   })
+
+  const startPoint = useRef<Point>({ x: 0, y: 0 })
+  const zenkainoTranslate = useRef<Point>({ x: 0, y: 0 })
+  const draggingElement = useRef<EventTarget & HTMLElement | null>(null)
+
+
+
   const handleDown = (e: React.MouseEvent<EventTarget & HTMLElement>): void => {
-    setFuyo({ isFuyo: true })
-    console.log(fuyo)
+    draggingElement.current = e.currentTarget
+    // 現在のtransform: translate()のx, y値を取得
+    const matrix = new DOMMatrix(getComputedStyle(draggingElement.current).transform)
+    console.log(matrix)
+    zenkainoTranslate.current = {
+      x: matrix.translateSelf().e,
+      y: matrix.translateSelf().f,
+    }
+
+    const draggableEl = document.getElementsByClassName('draggables') as HTMLCollectionOf<HTMLElement>
+    for (let i = 0; i < draggableEl.length; i++) {
+      draggableEl[i].style.zIndex = '1000'
+    }
+    draggingElement.current.style.position = 'realative'
+    draggingElement.current.style.zIndex = `1001`
+
+    // 押し込んだときのページの左上(0, 0)からのカーソルの座標
+    const x = e.pageX
+    const y = e.pageY
+    startPoint.current = {x, y}
+
+    // 押し込み時、isDownをtrueに切り替える
+    setMouseStatus(prevMouseStatus => ({
+      ...prevMouseStatus,
+      isUp: false,
+      isDown: true
+    }))
   }
   return [
+    {
+      mouseStatus,
+    },
     handleDown
   ]
 }
 
 export const DragDrop2 = () => {
-  const [handleDown] = useDragElemetns()
+  const [DraggingEl, handleDown] = useDragElemetns()
   return (
     <>
-      <div className="container">
-        <StyledApp>
-          <Draggable
-            id="red-box"
-            className="drag-and-drop red"
-            onMouseDown={handleDown}
-          />
-          <Draggable
-            id="blue-box"
-            className="drag-and-drop blue"
-            onMouseDown={handleDown}
-          />
-          <Draggable
-            id="yellow-box"
-            className="drag-and-drop yellow"
-            onMouseDown={handleDown}
-          />
-        </StyledApp>
-      </div>
+      <StyledApp>
+        <div className="container">
+          <div className="draggables">
+            <Draggable
+              id="red-box"
+              className={DraggingEl.mouseStatus.isDown ? 'drag-and-drop red drag' : 'drag-and-drop red'}
+              onMouseDown={handleDown}
+            />
+            <Draggable
+              id="blue-box"
+              className={DraggingEl.mouseStatus.isDown ? 'drag-and-drop blue drag' : 'drag-and-drop blue'}
+              onMouseDown={handleDown}
+            />
+            <Draggable
+              id="yellow-box"
+              className={DraggingEl.mouseStatus.isDown ? 'drag-and-drop yellow drag' : 'drag-and-drop yellow'}
+              onMouseDown={handleDown}
+            />
+          </div>
+        </div>
+      </StyledApp>
     </>
   )
 }
